45feae96426d5e3f82c4e3d9923cde3a1396adac
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-04-03 13:20:40 +0100

    Included gorilla schema and SignupForm  struct. Parsed into struct

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
+
 	"lenslocked.com/views"
 )
 
@@ -18,6 +20,11 @@ func NewUsers() *Users {
 
 type Users struct {
 	NewView *views.View
+}
+
+type SignupForm struct {
+	Email    string `schema: "email"`
+	Password string `schema: "password"`
 }
 
 //New renders form for new user account
@@ -35,9 +42,12 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostFormValue("email"))
-	fmt.Fprintln(w, r.PostForm["password"])
-	fmt.Fprintln(w, r.PostFormValue("password"))
-	fmt.Fprintln(w, "temp response")
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(w, form)
 }
