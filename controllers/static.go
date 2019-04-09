9cad765e31a14f16a13668bca3c7588cf06b8931
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-04-09 17:07:21 +0100

    included relational data using GORM

--- controllers/static.go
+++ controllers/static.go
@@ -10,10 +10,12 @@ func NewStatic() *Static {
 				Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),*/
 		Home:    views.NewView("bootstrap", "static/home"),
 		Contact: views.NewView("bootstrap", "static/contact"),
+		FAQ:     views.NewView("bootstrap", "static/faq"),
 	}
 }
 
 type Static struct {
 	Home    *views.View
 	Contact *views.View
+	FAQ     *views.View
 }
