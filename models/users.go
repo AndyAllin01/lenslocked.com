aeac01efc1fdfb69445db0310e49474075cf13df
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-04-20 20:08:12 +0100

    Implemented login authentication

--- models/users.go
+++ models/users.go
@@ -11,8 +11,10 @@ import (
 )
 
 var (
-	ErrNotFound  = errors.New("models: resource not found")
-	ErrInvalidID = errors.New("models: ID provided invalid")
+	ErrNotFound        = errors.New("models: resource not found")
+	ErrInvalidID       = errors.New("models: ID provided invalid")
+	ErrInvalidEmail    = errors.New("models: incorrect email provided")
+	ErrInvalidPassword = errors.New("models: incorrect password provided")
 )
 
 const userPwPepper = "secret-random-string-this-project"
@@ -51,6 +53,25 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 	db := us.db.Where("email = ?", email)
 	err := first(db, &user)
 	return &user, err
+}
+
+//Authenticate chceks password is correct for specified email address
+func (us *UserService) Authenticate(email, password string) (*User, error) {
+	foundUser, err := us.ByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(foundUser.PasswordHash), []byte(password+userPwPepper))
+
+	if err != nil {
+		switch err {
+		case bcrypt.ErrMismatchedHashAndPassword:
+			return nil, ErrInvalidPassword
+		default:
+			return nil, err
+		}
+	}
+	return foundUser, nil
 }
 
 //first will query using provided gorm DB and get first
