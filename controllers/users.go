ce288a9faf4db89f32b3b06270751c8e1de1354f
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-05-03 10:04:48 +0100

    implemented improved error handling in users.create action

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"lenslocked.com/rand"
@@ -37,14 +38,7 @@ type SignupForm struct {
 //New renders form for new user account
 //GET/signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	d:=views.Data{
-		Alert: &views.Alert{
-			Level: views.AlertLvlError,
-			Message: "ooooops!",
-		},
-		Yield: "Test yield!!!",
-	}
-	if err := u.NewView.Render(w, d); err != nil {
+	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
 }
@@ -53,9 +47,16 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //Create a new user accout
 //POST/signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
-		panic(err)
+		log.Println(err)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvlError,
+			Message: views.AlertMsgGeneric,
+		}
+		u.NewView.Render(w, vd)
+		return
 	}
 
 	user := models.User{
@@ -64,12 +65,16 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		Password: form.Password,
 	}
 	if err := u.us.Create(&user); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvlError,
+			Message: err.Error(),
+		}
+		u.NewView.Render(w, vd)
 		return
 	}
 	err := u.signIn(w, &user)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Redirect(w,r, "/login", http.StatusFound)
 		return
 	}
 	http.Redirect(w, r, "/cookietest", http.StatusFound)
