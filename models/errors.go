c5ebf53d984fbc51baa682d2c9835a468993f4c4
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-05-08 15:48:39 +0100

    implemented title and user id validators

--- models/errors.go
+++ models/errors.go
@@ -3,17 +3,19 @@ package models
 import "strings"
 
 const (
-	ErrNotFound          modelError = "models: resource not found"
-	ErrIDInvalid         modelError = "models: ID provided invalid"
-	ErrInvalidEmail      modelError = "models: incorrect email provided"
-	ErrPasswordIncorrect modelError = "models: incorrect password provided"
-	ErrEmailRequired     modelError = "Email address is required"
-	ErrEmailInvalid      modelError = "Email address is not valid"
-	ErrEmailTaken        modelError = "models: email address is already taken"
-	ErrPasswordTooShort  modelError = "models: password must be at least 8 characters"
-	ErrPasswordRequired  modelError = "models: password is required"
-	ErrRememberTooShort  modelError = "models: remember token must be at least 32 bytes"
-	ErrRememberRequired  modelError = "models: invlid remember token hassh"
+	ErrNotFound          modelError   = "models: resource not found"
+	ErrInvalidEmail      modelError   = "models: incorrect email provided"
+	ErrPasswordIncorrect modelError   = "models: incorrect password provided"
+	ErrEmailRequired     modelError   = "Email address is required"
+	ErrEmailInvalid      modelError   = "Email address is not valid"
+	ErrEmailTaken        modelError   = "models: email address is already taken"
+	ErrPasswordTooShort  modelError   = "models: password must be at least 8 characters"
+	ErrPasswordRequired  modelError   = "models: password is required"
+	ErrTitleRequired     modelError   = "models: title is required"
+	ErrIDInvalid         privateError = "models: ID provided invalid"	
+	ErrRememberTooShort  privateError = "models: remember token must be at least 32 bytes"
+	ErrRememberRequired  privateError = "models: invlid remember token hassh"
+	ErrUserIDRequired    privateError = "models: user ID is required"
 )
 
 type modelError string
@@ -28,4 +30,10 @@ func (e modelError) Public() string {
 	split := strings.Split(s, " ")
 	split[0] = strings.Title(split[0])
 	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
