b5d4468bcec7437cc185fece797cb2cd232729d0
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-05-22 15:21:01 +0100

    stopped running user middleware when serving static asset path prefixes

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"lenslocked.com/context"
 
@@ -19,6 +20,15 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		//if user is requesting a static asset or image, no need to look up
+		//user every time
+		if strings.HasPrefix(path, "/assets/") ||
+			strings.HasPrefix(path, "/images/") {
+			next(w, r)
+			return
+		}
+
 		//if user is logged in call
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
